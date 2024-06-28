21861dbbc58f864abe496d4d982f566e6ab095d4
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-06-28 17:01:48 +0200

    fixed main.py, added error handling

--- ngrep/main.py
+++ ngrep/main.py
@@ -7,9 +7,7 @@ token = os.environ["OPENAI_API_KEY"]
 
 client = openai.Client(api_key=token)
 
-
-def call(stdin, text):
-
+def generate_pattern(prompt):
     result = client.chat.completions.create(
         messages=[
             {
@@ -17,39 +15,29 @@ def call(stdin, text):
                 "content": "You just answer with python regex pattern, nothing else, do not use markdown, the result generated is passed directly into re.match",
             }, 
             {
-                "role": "user", "content":f" Generate a regex pattern that match this condition: {text}"
+                "role": "user", "content":f" Generate a regex pattern that match this condition: {prompt}"
             }
         ],
         model="gpt-4o",
     )
     result = result.choices[0].message.content
-    # print(result)
-    lines = stdin.split("\n")
-    for line in lines:
-        res = re.match(result, line)
-        if res is not None:
-            start, end = res.span()  # get the start and end positions of the match
-            # Print the line with the matched characters in color
-            colored_line = (
-                line[:start] +
-                '\033[91m' +  # ANSI code for red
-                line[start:end] +
-                '\033[0m' +  # ANSI code to reset color
-                line[end:]
-            )
-            print(colored_line)
-       
-        
-        
-    # print(type(result))
-    # x = re.search("^The.*Spain$", stdin)
-    # print(x)
-    # print(x)
+    return result
+    
 
-# total arguments
 arg = sys.argv[1]
-# print(len(arg))
 
-data = sys.stdin.read()
-call(data, arg)
+if sys.stdin.isatty():
+    print("No input data")
+    sys.exit(1)
+    
+
+    
+pattern = generate_pattern(arg)
+
+for line in sys.stdin.readlines():
+    res = re.match(pattern, line)
+    if res is not None:
+        print(line, end="")
+    
 sys.exit(0)
+    
