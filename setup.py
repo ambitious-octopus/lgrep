e89fe023c49d8608fb5a7fb9588fc39ce7692661
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-06-30 09:31:55 +0200

    changed names

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ngrep',
+    name='llgrep',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'ngrep=ngrep.main',
+            'llgrep=llgrep.main',
         ],
     },
     install_requires=[
